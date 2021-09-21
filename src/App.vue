ead847b0ab1ef5ddc4e32139dccb8be0bfe1dbd2
Lindsay Stewart <lj.stewart00@gmail.com>
2021-09-20 20:02:56 -0700

    pass props down

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
+    <Todos v-bind:todos="todos"/>
   </div>
 </template>
 <script>
+import Todos from './components/Todos';
 export default {
   name: 'app',
+  components: {
+    Todos
+  },
   data() {
     return {
       todos: [
